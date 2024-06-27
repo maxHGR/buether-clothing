565e1d118d7c0c3d5c1fff0a753b1d2bf8e6e7f4
maxHGR <maxhgr2@gmail.com>
2024-06-27 20:29:04 +0200

    pwa updates

--- app/(routes)/layout.js
+++ app/(routes)/layout.js
@@ -10,6 +10,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "Ether clothing",
   description: "",
+  manifest: "manifest.json"
 };
 
 export default function RootLayout({ children }) {
