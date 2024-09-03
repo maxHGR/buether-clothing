b8bb7fa6bcf14977f2c63c8d9550acbe96c8ccff
maxHGR <maxhgr2@gmail.com>
2024-09-03 19:21:23 +0200

    added expandable receipts

--- app/components/last-purchases/last-purchases.component.jsx
+++ app/components/last-purchases/last-purchases.component.jsx
@@ -8,6 +8,14 @@ import Image from 'next/image'
 export const LastPurchases = () => {
   const currentUser = useSelector(selectCurrentUser);
   const [receipts, setReceipts] = useState();
+  const [expandedReceipt, setExpandedReceipt] = useState("");
+
+  const toggleReceipt = (date) => {
+    console.log(date)
+    //if (expandedReceipt === date) return;
+
+    setExpandedReceipt(date);
+  }
   
   useEffect(() => {
     const retrieveReceipts = async() => {
@@ -39,23 +47,43 @@ export const LastPurchases = () => {
             hour: '2-digit', 
             minute: '2-digit',
           }); // Format the date with time
+          console.log(date)
             const totalPurchasePrice = item[1].purchasedItems.reduce((total, { price, quantity }) => total + (price * quantity), 0);
             // map() extracts the name, price,... from the passed in product object
             return (
-              <div key={date} className='mb-4 flex flex-col items-center bg-white rounded-lg p-4 shadow-md w-72  mx-auto'>
+              <div onClick={() => toggleReceipt(date)} key={date} className='mb-4 flex flex-col items-center bg-white rounded-lg p-4 shadow-md w-72  mx-auto'>
                 <h2 className='text-center p-2 mb-5'>{date}</h2>
-                {item[1].purchasedItems.map(({ name, id, imageUrl, price, quantity }) => (
-                  <div key={id} className='flex flex-col gap-y-2 items-center mb-2 border-b border-black w-44 p-1'>
-                    <Image src={imageUrl} alt={name} width={100} height={100}/>
-                    <div className='flex flex-col justify-center items-center text-sm'>
-                      <h3 className=''>{name}</h3>
-                      <p>Price: ${price}</p>
-                      <p>Quantity: {quantity}</p>
+                {
+                  expandedReceipt === date ? (
+                    null
+                  ) : (
+                    <button onClick={() => toggleReceipt(date)}>=</button>
+                  )
+                }
+                {
+                  expandedReceipt === date && (
+                    <>
+                  {item[1].purchasedItems.map(({ name, id, imageUrl, price, quantity }) => {
+                    return (
+                      <div key={id} className='flex flex-col gap-y-2 items-center mb-2 border-b border-black w-44 p-1'>
+                      <Image src={imageUrl} alt={name} width={100} height={100}/>
+                      <div className='flex flex-col justify-center items-center text-sm'>
+                        <h3 className=''>{name}</h3>
+                        <p>Price: ${price}</p>
+                        <p>Quantity: {quantity}</p>
+                      </div>
                     </div>
-                  </div>
-                ))}
+                    )
+                })}
                 <h3 className='text-center'>Total Purchase Price: ${totalPurchasePrice.toFixed(2)}</h3>
+                    </>
+                  )
+
+
+                
+                }
               </div>
+              
             );
           })}
         </div>
@@ -64,42 +92,6 @@ export const LastPurchases = () => {
       )}
     </div>
   );
-
-
-
-  return (
-    <div>
-      { receipts ? (
-        <>
-          {receipts.map((item) => {
-            const date = item[0];
-            // map() extracts the name, price,... from the passed in product object
-            return (
-            <div key={date} className='mb-4 flex flex-col items-center'>
-              <h2 className='text-center'>{date}</h2>
-              {item[1].purchasedItems.map(({ name, id, imageUrl, price, quantity }) => (
-                <div key={id} className='flex flex-col items-center mb-2 border w-44 p-1'>
-                  <Image src={imageUrl} alt={name} width={40} height={40}/>
-                  <div className='flex flex-col justify-center items-center'>
-                    <h3 className=''>{name}</h3>
-                    <p>Price: ${price}</p>
-                    <p>Quantity: {quantity}</p>
-                  </div>
-                </div>
-              ))}
-            </div>
-          )
-          }
-          )}
-
-        </>
-      ) : (
-        <p>No receipts</p>
-      )}
-
-    	<button onClick={receiptHandler}>Get Receipts</button>
-    </div>
-  )
 }
 
 export default LastPurchases
