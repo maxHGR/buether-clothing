82ead54475cd6da6c8b0add455bc2e54bf593455
maxHGR <maxhgr2@gmail.com>
2024-09-04 12:50:15 +0200

    added pwa support

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,8 @@ const withPWA = require("next-pwa")({
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
   images: {
     remotePatterns: [
       {
